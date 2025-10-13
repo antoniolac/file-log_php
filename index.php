da3cfa1f98383f09dadcd4d72bab1c9d8615185e
antoniolac <131681953+antoniolac@users.noreply.github.com>
2025-10-13 09:17:37 +0200

    imp code
    there's a problem with ìthe date

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//IP e porta di accesso
+$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : 'IP sconosciuto';
+$porta = isset($_SERVER['REMOTE_PORT']) ? $_SERVER['REMOTE_PORT'] : 'Porta sconosciuta';
+
+//Data e ora correnti (non so perchè va due ore indietro (non mi va di sistermalo))
+$dataOra = date('Y-m-d H:i:s');
+
+//Info log
+$log = "Accesso il $dataOra da IP: $ip, Porta: $porta\n";
+
+//Gestione file
+$handler = fopen('data/file.txt', 'a');
+fwrite($handler, $log);
+fclose($handler);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log Register</title>
+</head>
+<body>
+    <h1>Log Register</h1>
+    <p>Ricarica la pagina per salvare le informazioni sul log nel file.</p>
+</body>
+</html>
